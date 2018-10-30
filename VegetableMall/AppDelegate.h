5d1a663fafa6380feb7e6b2671cc6e08ce1ff67e
cuining_c <380693205@qq.com>
2018-10-30 10:22:28 +0800

    Initial Commit

---
+++ VegetableMall/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VegetableMall
+//
+//  Created by mading shouyou on 2018/10/30.
+//  Copyright © 2018 madingshouyou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
